e62c17ede9129be377d81a9aed6866efb4b2cf75
ryantsui1109 <68107903+ryantsui1109@users.noreply.github.com>
2022-06-16 22:23:55 +0800

    some improvements

--- render.js
+++ render.js
@@ -36,6 +36,8 @@ function startAction(optmode, opt) {
     // 生成指令
     let hasRadio = true
     let hasFile = false
+    let targetHasFile = ['sideload', 'flash', 'install', 'push', 'boot']
+    let targetHasNoRadio = ['sideload', 'install', 'push', 'boot']
     let param = opt.split('-')[0]
     let params = []
 
@@ -44,10 +46,10 @@ function startAction(optmode, opt) {
     if (param == 'power' || param == 'system') { param = 'reboot' }
     if (param == 'active') { param = 'set_active' }
 
-    if (param == 'sideload' || param == 'install' || param == 'push' || param == 'boot') { hasRadio = false }
+    if (targetHasNoRadio.includes(param)) { hasRadio = false }
     if (optmode == 'adb' && param == 'reboot') { hasRadio = true }
 
-    if (param == 'sideload' || param == 'flash' || param == 'install' || param == 'push' || param == 'boot') { hasFile = true }
+    if (targetHasFile.includes(param)) { hasFile = true }
 
     if (param == 'erase' && document.getElementById('use_format').checked) {
         param = 'format'
@@ -81,60 +83,37 @@ function startAction(optmode, opt) {
     }
     if (param == 'push') { params.push('/sdcard/') }
 
-
+    let cmd = ''
     if (optmode == 'fastboot') {
-        let cmd = fastbootPath
-        for (x of params) {
-            cmd += ' '
-            cmd += x
-        }
-        child.exec(cmd, (error, stdout, stderr) => {
-            if (error) {
-                let errmsg = ''
-                for (x of error.message.split('\n')) {
-                    errmsg += (`<p>${x}</p>`)
-                }
-                console.log(errmsg)
-                displayAlert(errmsg, 'danger')
-            } else {
-                let logmsg = ''
-                for (x of stderr.split('\n')) {
-                    logmsg += (`<p>${x}</p>`)
-                }
-                displayAlert(logmsg, 'success')
-            }
-
-        })
-
+        cmd = fastbootPath
     }
     if (optmode == 'adb') {
-        let cmd = adbPath
-        for (x of params) {
-            cmd += ' '
-            cmd += x
-        }
-        child.exec(cmd, (error, stdout, stderr) => {
-            if (error) {
-                let errmsg = ''
-                for (x of error.message.split('\n')) {
-                    errmsg += (`<p>${x}</p>`)
-                }
-                displayAlert(errmsg, 'danger')
-            } else {
-                let logmsg = ''
-                for (x of stderr.split('\n')) {
-                    logmsg += (`<p>${x}</p>`)
-                }
-                displayAlert(logmsg, 'success')
-            }
-        })
+        cmd = adbPath
     }
+    for (x of params) {
+        cmd += ' '
+        cmd += x
+    }
+    child.exec(cmd, (error, stdout, stderr) => {
+        if (error) {
+            let errmsg = ''
+            for (x of error.message.split('\n')) {
+                errmsg += (`<p>${x}</p>`)
+            }
+            displayAlert(errmsg, 'danger')
+        } else {
+            let logmsg = ''
+            for (x of stderr.split('\n')) {
+                logmsg += (`<p>${x}</p>`)
+            }
+            displayAlert(logmsg, 'success')
+        }
+    })
 }
 
 
 
 jQuery(function() {
-
     function renderNavbar(items) {
         for (let x in items) {
             const item = items[x]
@@ -144,10 +123,8 @@ jQuery(function() {
                     <a class="nav-link" href="javascript:void(0)" id="${item.navbar.toLowerCase()}" onclick="switchOprMode('${item.navbar.toLowerCase()}')">${item.navbar}</a>
                 </li>
                 `);
-
         }
     }
-
 
     function renderBody(oprs) {
         for (let x in oprs) {
@@ -225,7 +202,6 @@ jQuery(function() {
                     <label class="form-check-label" for="flexCheckDefault">
                         ${opt[2]}
                 </label>
-                    
                     `
                 )
             }
@@ -256,19 +232,13 @@ jQuery(function() {
                         `<h5 class="card-subtitle mb-2 text-muted">${card.subtitle}</h5>`
                     )
                 }
-
                 processOpt(card.content)
                     // 開始按鈕
                 $('body').find(`#${card.name}`).append(`
                 <button type="button" class="btn btn-primary" id="${card.name}-btn" onclick="startAction('${currentOpr.toLowerCase()}','${card.name}')">${startActionBtn}</button>`)
-
             }
         }
     }
-
-
-
-
     renderNavbar(oprs)
     renderBody(oprs)
     renderCards(oprs)
