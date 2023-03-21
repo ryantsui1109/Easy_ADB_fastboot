00370080e63c692b4d0e728b057aed3400ac2013
ryantsui1109 <68107903+ryantsui1109@users.noreply.github.com>
2023-03-22 00:03:43 +0800

    rewrited eaf_builder

--- scripts/eaf_builder.js
+++ scripts/eaf_builder.js
@@ -13,161 +13,130 @@ const { type } = require("os");
 const { https } = require("follow-redirects");
 const builder = require("electron-builder");
 const pkgInfo = require("../package.json");
-function buildAPP(args) {
-  const buildConfig = require("./build.json");
-
-  if (!args.a) {
-    buildConfig.appId = "io.github.ryantsui1109.eaf";
-    if (args.c != "release") {
-      buildConfig.appId += "." + args.c;
-    }
-  } else {
-    buildConfig.appId = args.a;
-  }
-  if (args.b) {
-    console.log("Start building.");
-    try {
-      mkdirSync("dist");
-    } catch (error) {
-      console.log('"dist" exist!');
-    }
-
-    writeFileSync(
-      "dist/buildInfo.txt",
-      `Version: ${pkgInfo.version}\nUpdate Index: ${args.i}`
-    );
-    builder.build({
-      config: buildConfig,
-    });
-  }
-}
-function configure(args) {
-  const config = require("../default_config.json");
-  const updaterStatus = require("../default_updaterStatus.json");
-  const cleanBeforeBuild = ["updaterStatus.json", "config.json", "update.exe"];
-  cleanBeforeBuild.forEach((e) => {
-    if (existsSync(e)) {
-      rmSync(e);
-    }
-  });
-  config.channel = args.c;
-  config.updateIndex = args.i;
-  writeFileSync("config.json", JSON.stringify(config, null, "  "));
-  writeFileSync(
-    "updaterStatus.json",
-    JSON.stringify(updaterStatus, null, "  ")
-  );
-}
-
-function checkArgs(arguments) {
-  if (!arguments.d) {
-    let conditions = 0;
-    arguments.c ? conditions++ : console.error("Update channel must be set.");
-    Number.isInteger(Number(arguments.i))
-      ? conditions++
-      : console.error("Update index must be set as an integer.");
-
-    if (conditions == 2) {
-      return true;
-    } else {
-      console.log(
-        '\nFor more help, please type "node .\\scripts\\eaf_builder.js -h".'
-      );
-    }
-  }
-}
+const args = require("args-parser")(process.argv);
+const buildInfo = require("./build.json");
+const appConfig = require("../default_config.json");
+const updaterStatus = require("../default_updaterStatus.json");
 const downloadFile = (url, dest) =>
   new Promise((resolve, reject) => {
     const file = createWriteStream(dest);
     https.get(url, (res) => {
       console.log(`Downloading from ${url}`);
       res.pipe(file);
-
       file.on("finish", () => {
-        resolve("done!");
+        resolve("Download completed");
       });
     });
   });
-function main() {
-  const args = require("args-parser")(process.argv);
 
+async function main() {
   if (args.h) {
     console.log("");
-    console.log("=== Easy ADB and Fastboot Build Script===");
+    console.log("=== Easy ADB and Fastboot Build Script ===");
     console.log("");
     console.log("-h    Display this message.");
     console.log("");
-    console.log(
-      "-a    Set AppID for this build (default: io.github.ryantsui1109.[channel])."
-    );
     console.log("-b    Build with configs.");
     console.log("-c    Set update channel for this build.");
-    console.log("-d    Download platform-tools");
+    console.log("-d    Download platform-tools (Automatically detect OS)");
     console.log("-i    Set update index for this build.");
     console.log("");
     console.log("Example: node .\\script\\eaf_builder.js -c=beta -i=6");
   } else {
     if (args.d) {
-      console.log("Start downloading platform-tools");
-      let downloadProcess;
-      const platform = type();
-      switch (platform) {
-        case "Linux":
-          downloadProcess = downloadFile(
-            "https://dl.google.com/android/repository/platform-tools-latest-linux.zip",
-            "platform-tools.zip"
-          );
-          break;
-        case "Windows_NT":
-          downloadProcess = downloadFile(
-            "https://dl.google.com/android/repository/platform-tools-latest-windows.zip",
-            "platform-tools.zip"
-          );
-          break;
-        default:
-          console.log("Platform currently not supported!");
-          break;
-      }
-      downloadProcess.then(() => {
-        if (existsSync("platform-tools.zip")) {
-          console.log('Unzipping platform-tools.zip')
-          const unzipProgress = createReadStream("platform-tools.zip").pipe(
-            unzipper.Extract({ path: "." })
-          );
-          unzipProgress.on("close", () => {
-            switch (platform) {
-              case "Linux":
-                rmSync("platform-tools-linux", {
-                  recursive: true,
-                  force: true,
-                });
-                renameSync("platform-tools", "platform-tools-linux");
-                break;
-              case "Windows_NT":
-                rmSync("platform-tools-win", {
-                  recursive: true,
-                  force: true,
-                });
-                renameSync("platform-tools", "platform-tools-win");
-                break;
-              default:
-                break;
-            }
-          });
+      const downloadPT = new Promise((resolve, reject) => {
+        // download platform-tools
+        console.log("Start downloading platform-tools");
+        let downloadProcess;
+        const platform = type();
+        switch (platform) {
+          case "Linux":
+            downloadProcess = downloadFile(
+              "https://dl.google.com/android/repository/platform-tools-latest-linux.zip",
+              "platform-tools.zip"
+            );
+            break;
+          case "Windows_NT":
+            downloadProcess = downloadFile(
+              "https://dl.google.com/android/repository/platform-tools-latest-windows.zip",
+              "platform-tools.zip"
+            );
+            break;
+          default:
+            reject("Platform currently not supported!");
+            break;
         }
+        downloadProcess.then((result) => {
+          console.log(result);
+          if (existsSync("platform-tools.zip")) {
+            console.log("Unzipping platform-tools.zip");
+            const unzipProgress = createReadStream("platform-tools.zip").pipe(
+              unzipper.Extract({ path: "." })
+            );
+            unzipProgress.on("close", () => {
+              switch (platform) {
+                case "Linux":
+                  rmSync("platform-tools-linux", {
+                    recursive: true,
+                    force: true,
+                  });
+                  renameSync("platform-tools", "platform-tools-linux");
+                  break;
+                case "Windows_NT":
+                  rmSync("platform-tools-win", {
+                    recursive: true,
+                    force: true,
+                  });
+                  renameSync("platform-tools", "platform-tools-win");
+                  break;
+                default:
+                  break;
+              }
+            });
+          }
+          resolve("Unzip finished");
+        });
       });
+      await downloadPT
+        .then((res) => {
+          console.log(res);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
     }
-    if (checkArgs(args)) {
-      console.log("");
-      console.log("Update channel: " + args.c);
-      console.log("Update index: " + args.i);
-      console.log("-----------------------------------------");
-      console.log("");
-      console.log("Start configuring.");
-      configure(args);
 
-      buildAPP(args);
-    }
+    const checkArgs = new Promise((resolve, reject) => {
+      let result = [];
+      let conditions = 0;
+      args.c ? ++conditions : result.push("Update Channel");
+      !isNaN(args.i) ? ++conditions : result.push("Update Index");
+      conditions >= 2
+        ? resolve("Configurations check passed")
+        : reject(`${result} shoud be set`);
+    });
+    await checkArgs
+      .then((result) => {
+        console.log(result);
+        appConfig.channel = args.c;
+        appConfig.updateIndex = args.i;
+        writeFileSync("config.json", JSON.stringify(appConfig, null, "  "));
+        writeFileSync(
+          "updaterStatus.json",
+          JSON.stringify(updaterStatus, null, "  ")
+        );
+        if (args.b) {
+          console.log("Start building");
+          if (args.c !== "release") {
+            buildInfo.appId += "." + args.c;
+          }
+
+          builder.build({ config: buildInfo });
+        }
+      })
+      .catch((result) => {
+        console.log(result);
+      });
   }
 }
 main();
