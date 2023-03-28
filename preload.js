5bb9e7f286ed8bb56afb08812f8f5b510cef6b8a
ryantsui1109 <68107903+ryantsui1109@users.noreply.github.com>
2023-03-28 23:11:55 +0800

    trying to use preload.js

---
+++ preload.js
@@ -0,0 +1,8 @@
+// const { contextBridge } = require("electron");
+
+// contextBridge.exposeInMainWorld("versions", {
+//   chrome: () => process.versions.chrome,
+//   electron: () => process.versions.electron,
+//   appVersion: () => process.version,
+//   osVersion: () => process.getSystemVersion(),
+// });
